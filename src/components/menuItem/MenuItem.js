75920eaa28dfbfa4f8848ca2e3017b9d869a1e27
Mateusz Mamica <mateusz.mamica18@gmail.com>
2024-08-27 22:26:54 +0200

    add global styles '

---
+++ src/components/menuItem/MenuItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MenuItem = () => {
+  return (
+    <>
+      <h1>CartItem</h1>
+    </>
+  );
+};
---
+++ src/themes/GlobalStyles.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+import RedHatTextRegular from "../assets/fonts/RedHatText-VariableFont_wghtRedHatText-VariableFont_wght.ttf";
+import RedHatTextItalic from "../assets/fonts/RedHatText-Italic-VariableFont_wght.ttf";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    @font-face {
+        font-family: 'RedHatText';
+        src: url(${RedHatTextRegular}) format('truetype');
+        font-weight: 100 900;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: 'RedHatText';
+        src: url(${RedHatTextItalic}) format('truetype');
+        font-weight: 100 900;
+        font-style: italic;
+    }
+
+    body {
+        font-family: "RedHatText";
+    }
+`;
+
+export default GlobalStyle;

65f7241444a6119281654a7eaeb38553540137e5
Mateusz Mamica <mateusz.mamica18@gmail.com>
2024-08-28 16:01:18 +0200

    Add menu item cart mobile design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
+import styled from "styled-components";
 import { MenuItem } from "./components/menuItem/MenuItem";
-function App() {
+
+const MenuItemWrapper = styled.div`
+  padding: 1.5rem 1rem;
+`;
+
+const Heading = styled.h1`
+  margin-bottom: 2rem;
+`;
+
+const App = () => {
   return (
     <>
-      <h1>Desserts</h1>
-      <MenuItem />
+      <MenuItemWrapper>
+        <Heading>Desserts</Heading>
+        <MenuItem />
+      </MenuItemWrapper>
     </>
   );
-}
+};
 
 export default App;
--- src/components/menuItem/MenuItem.js
+++ src/components/menuItem/MenuItem.js
@@ -1,9 +1,75 @@
 import React from "react";
+import data from "../../data.json";
+import styled from "styled-components";
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: auto;
+`;
+
+const Photo = styled.img`
+  width: 100%;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  bottom: 0%;
+  left: 50%;
+  transform: translate(-50%, 30%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  font-weight: bold;
+  background-color: #fff;
+  padding: 0.5rem 1.75rem;
+  border: 2px solid ${({ theme }) => theme.colors.rose300};
+  border-radius: 25px;
+  z-index: 1;
+`;
+
+const Price = styled.p`
+  color: ${({ theme }) => theme.colors.red};
+  font-weight: bold;
+`;
+
+const Name = styled.p`
+  font-weight: bold;
+  font-size: 1rem;
+`;
+
+const Category = styled.p`
+  color: ${({ theme }) => theme.colors.rose300};
+  font-weight: 700;
+`;
+
+const DataInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+  margin: 1.5rem 0;
+`;
 
 export const MenuItem = () => {
   return (
     <>
-      <h1>CartItem</h1>
+      {data.map((item, index) => (
+        <div key={index}>
+          <ImageWrapper>
+            <Photo src={item.image.mobile} alt={item.name} />
+            <Button>
+              <img src="/images/icon-add-to-cart.svg" alt="Add to Cart" />
+              Add to Cart
+            </Button>
+          </ImageWrapper>
+          <DataInfo>
+            <Category>{item.category}</Category>
+            <Name>{item.name}</Name>
+            <Price>${item.price.toFixed(2)}</Price>
+          </DataInfo>
+        </div>
+      ))}
     </>
   );
 };
